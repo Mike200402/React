4121ef55c1f7125f15f56fcd6aaa61ba59ccb126
Miguel Flores <dragonex0020@gmail.com>
2024-04-24 23:33:48 -0400

    Prueba final espero

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+import RickAndMortyApp from './RickAndMortyApp.jsx'
+import Home from './components/Home.jsx'
+const router=createHashRouter([
+  {
+    path:"/",
+    element:<Home/>,
+  },
+  {
+    path:"/character",
+    element:<RickAndMortyApp/>,
+  },{
+    path:"/",
+    element:<p>ERROR 404 Pagina Inexistente</p> ,
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
